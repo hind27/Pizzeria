797f670db83fc7026107495e717da8f5b450078a
HindAnwar <hendanwar27@outlook.com>
2021-01-26 23:15:59 +0200

    git branch -M main

---
+++ main.js
@@ -0,0 +1,18 @@
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
+ 
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+$(document).ready(function(){
+  $('.navbar-dark > button').on('click', function(){
+      $('.navbar-dark').toggleClass('color-changed');
+  });
+  });
